9a8d1284643fe600dc238d8dea7cb6bba392348a
xuwendi <1281820940@qq.com>
2022-10-08 19:13:55 +0800

    二次提交

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,5 +1,7 @@
 public class Test {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git2");
+        System.out.println("hello git3");
     }
 }
